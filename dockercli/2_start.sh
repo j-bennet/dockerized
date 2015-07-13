913c139e67e53a0fc4e7028062718292d224b851
Iryna Cherniavska <i.chernyavska@gmail.com>
2015-07-12 21:19:22 -0700

    Switched branch to master.

--- dockercli/2_start.sh
+++ dockercli/2_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -it -e CODE_BRANCH=run-with-tty -v /var/run/docker.sock:/run/docker.sock -v $(which docker):/bin/docker dcli
+docker run --rm -it -e CODE_BRANCH=master -v /var/run/docker.sock:/run/docker.sock -v $(which docker):/bin/docker dcli
